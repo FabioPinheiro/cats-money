6366d9e670cec451901cbd1b70af77e5d5799ada
FabioPinheiro <fabiomgpinheiro@gmail.com>
2020-04-20 21:47:26 +0100

    8

--- src/main/scala/app/fmgp/money/Demo.scala
+++ src/main/scala/app/fmgp/money/Demo.scala
@@ -8,7 +8,36 @@ object Demo extends App {
 
   type AUX = Int | Unit
 
-  //FIXME
-  val maybeBug: MoneyTree[MoneyY[AUX]] = (MoneyY(Int): AUX).pure[MoneyTree]
+  val endlessCompilation: MoneyTree[MoneyY[AUX]] = (MoneyY(Int): AUX).pure[MoneyTree]
 
 }
+
+// after a `jstack` on the sbt process
+
+// "pool-45-thread-2" #358 prio=5 os_prio=0 cpu=905000.25ms elapsed=920.64s tid=0x00007fee32316800 nid=0x21306 runnable  [0x00007fed49da0000]
+//    java.lang.Thread.State: RUNNABLE
+// 	at dotty.tools.dotc.core.Types$NamedType.denot(Types.scala:1959)
+// 	at dotty.tools.dotc.core.TypeApplications$.safeDealias$extension(TypeApplications.scala:244)
+// 	at dotty.tools.dotc.core.TypeApplications$.appliedTo$extension(TypeApplications.scala:284)
+// 	at dotty.tools.dotc.core.Types$AppliedType.derivedAppliedType(Types.scala:3917)
+// 	at dotty.tools.dotc.core.Types$TypeMap.derivedAppliedType(Types.scala:4831)
+// 	at dotty.tools.dotc.core.Types$TypeMap.mapOver(Types.scala:4889)
+// 	at dotty.tools.dotc.core.Substituters.substParam(Substituters.scala:143)
+// 	at dotty.tools.dotc.core.Contexts$Context.substParam(Contexts.scala:75)
+// 	at dotty.tools.dotc.core.Substituters$SubstParamMap.apply(Substituters.scala:189)
+// ...
+// 	at dotty.tools.dotc.core.Substituters$SubstParamMap.apply(Substituters.scala:189)
+// 	at dotty.tools.dotc.core.Types$TypeMap.op$4(Types.scala:4881)
+// 	at dotty.tools.dotc.core.Types$TypeMap.mapArgs$1(Types.scala:4881)
+// 	at dotty.tools.dotc.core.Types$TypeMap.mapOver(Types.scala:4889)
+// 	at dotty.tools.dotc.core.Substituters.substParam(Substituters.scala:143)
+// 	at dotty.tools.dotc.core.Contexts$Context.substParam(Contexts.scala:75)
+// 	at dotty.tools.dotc.core.Substituters$SubstParamMap.apply(Substituters.scala:189)
+// 	at dotty.tools.dotc.core.Types$TypeMap.op$4(Types.scala:4881)
+// 	at dotty.tools.dotc.core.Types$TypeMap.mapArgs$1(Types.scala:4881)
+// 	at dotty.tools.dotc.core.Types$TypeMap.mapOver(Types.scala:4889)
+// 	at dotty.tools.dotc.core.Substituters.substParam(Substituters.scala:143)
+// 	at dotty.tools.dotc.core.Contexts$Context.substParam(Contexts.scala:75)
+// 	at dotty.tools.dotc.core.Substituters$SubstParamMap.apply(Substituters.scala:189)
+// 	at dotty.tools.dotc.core.Types$TypeMap.op$4(Types.scala:4881)
+//
