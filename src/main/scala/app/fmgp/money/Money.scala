13b05a3222646e221e2a2fa6e6c6cdd00beb5894
FabioPinheiro <fabiomgpinheiro@gmail.com>
2020-04-20 23:26:11 +0100

    9

--- src/main/scala/app/fmgp/money/Money.scala
+++ src/main/scala/app/fmgp/money/Money.scala
@@ -1,11 +1,12 @@
 package app.fmgp.money
 
 import cats.Show
-
 sealed abstract case class MoneyY[+T](currency: T)
-
 sealed abstract class MoneyTree[+T]
-
 object MoneyTreeInstances {
+  //Takes 50s to give a Type Mismatch Error on endlessCompilation
+  //given MoneyTreeShow[T](using Show[T]) as Show[MoneyTree[T]] = ???
+
+  //Takes more then 10 mins to give the  Type Mismatch Error on endlessCompilation
   implicit def MoneyTreeShow[T](implicit showT: Show[T]): Show[MoneyTree[T]] = ???
 }
