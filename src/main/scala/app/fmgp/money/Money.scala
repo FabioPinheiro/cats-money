521e4902c64bfeedcb0a1d321808e5b9f9074099
FabioPinheiro <fabiomgpinheiro@gmail.com>
2020-04-20 21:07:00 +0100

    7

--- src/main/scala/app/fmgp/money/Money.scala
+++ src/main/scala/app/fmgp/money/Money.scala
@@ -1,6 +1,6 @@
 package app.fmgp.money
 
-import cats.{Show, Functor, Monad, Traverse}
+import cats.Show
 
 sealed abstract case class MoneyY[+T](currency: T)
 
@@ -8,7 +8,4 @@ sealed abstract class MoneyTree[+T]
 
 object MoneyTreeInstances {
   implicit def MoneyTreeShow[T](implicit showT: Show[T]): Show[MoneyTree[T]] = ???
-  implicit val MoneyTreeFunctor: Functor[MoneyTree] = ???
-  implicit val MoneyTreeMonad: Monad[MoneyTree] = ???
-  val MoneyTreeTraverse: Traverse[MoneyTree] = ???
 }
